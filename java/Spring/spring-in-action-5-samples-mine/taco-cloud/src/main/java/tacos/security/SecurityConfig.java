68a65794c9f683a50d273193df718bc945d7253d
delay <652559480@qq.com>
2019-08-30 16:38:14 +0800

    taco-cloud

---
+++ java/Spring/spring-in-action-5-samples-mine/taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package tacos.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
